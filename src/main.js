c112a452462b92dddd5c985f17d5fa8b09ad9e2b
MiaoMii <miaomiim@gmail.com>
2022-08-03 18:29:14 +0800

    feat: 代码提交

--- src/main.js
+++ src/main.js
@@ -2,9 +2,12 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
-
+import VueForm from "@lljj/vue-json-schema-form";
+import ElementUI from "element-ui";
+import "element-ui/lib/theme-chalk/index.css";
 Vue.config.productionTip = false;
-
+Vue.component("VueForm", VueForm);
+Vue.use(ElementUI);
 new Vue({
   router,
   store,
